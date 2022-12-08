29595f932e21665bcae694834274ede553393c48
ali <aliaitbella3@gmail.com>
2022-12-08 21:43:25 +0100

    Fin Index.html et style.css

---
+++ public/js/script.js
@@ -0,0 +1,61 @@
+let articles=[
+    {
+        id:1,
+        titre:"Le web",
+        auteur:"Brahime",
+        date:new Date("1/2/2022 12:24:00"),
+        contenu:[
+            "Inventore similique voluptatibus repellat accusamus reiciendis porro perspiciatis, iste tempore laborum repudiandae officiis quibusdam nostrum alias cum nemo ea optio in odit ut possimus velit nulla magni ipsum. Dolorum, reiciendis.",
+            "Inventore similique voluptatibus repellat accusamus reiciendis porro perspiciatis, iste tempore laborum repudiandae officiis quibusdam nostrum alias cum nemo ea optio in odit ut possimus velit nulla magni ipsum. Dolorum, reiciendis.Inventore similique voluptatibus repellat accusamus reiciendis porro perspiciatis, iste tempore laborum repudiandae officiis quibusdam nostrum alias cum nemo ea optio in odit ut possimus velit nulla magni ipsum. Dolorum, reiciendis.",
+            "Inventore similique voluptatibus repellat accusamus reiciendis porro perspiciatis tempore laborum repudiandae officiis quibusdam nostrum alias cum nemo ea optio in odit ut possimus velit nulla magni ipsum. Dolorum, reiciendis."
+        ]
+
+    },
+    {
+        id:2,
+        titre:"HTML et CSS",
+        auteur:"Imane",
+        date:new Date("16/4/2022 9:24:00"),
+        contenu:[
+            "Liste tempore laborum repudiandae officiis quibusdam nostrum alias cum nemo ea optio in odit ut possimus velit nulla magni ipsum. Dolorum, reiciendis.",
+            "Inventore similique voluptatibus repellat accusamus reiciendis porro perspiciatis, iste tempore laborum repudiandae officiis quibusdam nostrum alias cum nemo ea optio in odit ut possimus velit nulla magni ipsum. Dolorum, reiciendis.Inventore similique voluptatibus repellat accusamus reiciendis porro perspiciatis, iste tempore laborum repudiandae officiis quibusdam nostrum alias cum nemo ea optio in odit ut possimus velit nulla magni ipsum. Dolorum, reiciendis.",
+            "Inventore similique voluptatibus repellat accusamus reiciendis porro perspiciatis tempore laborum repudiandae officiis quibusdam nostrum alias cum nemo ea optio in odit ut possimus velit nulla magni ipsum. Dolorum, reiciendis."
+        ]
+
+    },
+    {
+        id:3,
+        titre:"Le Javascript",
+        auteur:"Brahime",
+        date:new Date("15/10/2022 12:24:00"),
+        contenu:[
+            "laborum repudiandae officiis quibusdam nostrum alias cum nemo ea optio in odit ut possimus velit nulla magni ipsum. Dolorum, reiciendis.",
+            "Inventore similique voluptatibus repellat accusamus reiciendis porro perspiciatis, iste tempore laborum repudiandae officiis quibusdam nostrum alias cum nemo ea optio in odit ut possimus velit nulla magni ipsum. Dolorum, reiciendis.Inventore similique voluptatibus repellat accusamus reiciendis porro perspiciatis, iste tempore laborum repudiandae officiis quibusdam nostrum alias cum nemo ea optio in odit ut possimus velit nulla magni ipsum. Dolorum, reiciendis."
+        ]
+
+    },
+    {
+        id:4,
+        titre:"Ajax",
+        auteur:"Imane",
+        date:new Date("22/11/2022 12:24:00"),
+        contenu:[
+            "Inventore similique voluptatibus repellat accusamus reiciendis porro perspiciatis, iste tempore laborum repudiandae officiis quibusdam nostrum alias cum nemo ea optio in odit ut possimus velit nulla magni ipsum. Dolorum, reiciendis.",
+            "Inventore similique voluptatibus repellat accusamus reiciendis porro perspiciatis, iste tempore laborum repudiandae officiis quibusdam nostrum alias cum nemo ea optio in odit ut possimus velit nulla magni ipsum. Dolorum, reiciendis.Inventore similique voluptatibus repellat accusamus reiciendis porro perspiciatis, iste tempore laborum repudiandae officiis quibusdam nostrum alias cum nemo ea optio in odit ut possimus velit nulla magni ipsum. Dolorum, reiciendis.",
+            "Inventore similique voluptatibus repellat accusamus reiciendis porro perspiciatis tempore laborum repudiandae officiis quibusdam nostrum alias cum nemo ea optio in odit ut possimus velit nulla magni ipsum. Dolorum, reiciendis."
+        ]
+
+    },
+    {
+        id:5,
+        titre:"Le web",
+        auteur:"Brahime",
+        date:new Date("24/11/2022 23:24:00"),
+        contenu:[
+            "Inventore similique voluptatibus repellat accusamus reiciendis porro perspiciatis, iste tempore laborum repudiandae officiis quibusdam nostrum alias cum nemo ea optio in odit ut possimus velit nulla magni ipsum. Dolorum, reiciendis.",
+            "Inventore similique voluptatibus repellat accusamus reiciendis porro perspiciatis, iste tempore laborum repudiandae officiis quibusdam nostrum alias cum nemo ea optio in odit ut possimus velit nulla magni ipsum. Dolorum, reiciendis.Inventore similique voluptatibus repellat accusamus reiciendis porro perspiciatis, iste tempore laborum repudiandae officiis quibusdam nostrum alias cum nemo ea optio in odit ut possimus velit nulla magni ipsum. Dolorum, reiciendis.",
+            "Inventore similique voluptatibus repellat accusamus reiciendis porro perspiciatis tempore laborum repudiandae officiis quibusdam nostrum alias cum nemo ea optio in odit ut possimus velit nulla magni ipsum. Dolorum, reiciendis."
+        ]
+
+    },
+]
